62c24e246f5185b1db41a939dd37b6a1f69666ff
马凌博 <grawhawk@gmail.com>
2015-06-04 23:40:14 +0800

    增加实体

---
+++ trunk/BPMProject/BPM.Entity/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPM.Entity
+{
+    public class Employee
+    {
+        public double ID { get; set; }
+        public string Name { get; set; }
+        public double DeptID { get; set; }
+        public double Attribute { get; set; }
+        public int Age { get; set; }
+        public string AliasName { get; set; }
+        public string Password { get; set; }
+        public string KeyString { get; set; }
+        public int Sex { get; set; }
+        public string TelNo { get; set; }
+        public double Rank { get; set; }
+        public string Remark { get; set; }
+    }
+}
---
+++ trunk/BPMProject/BPM.Entity/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPM.Entity
+{
+    public class Role
+    {
+        public double id { get; set; }
+        public string  Name { get; set; }
+        public double EmployeeID { get; set; }
+        public string AccessMask { get; set; }
+        public string Remark { get; set; }
+    }
+}
