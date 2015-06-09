d70f75e1047cfa58d254307a805b2565c6297ad0
马凌博 <grawhawk@gmail.com>
2015-06-09 23:20:27 +0800

    修改装备维修流程

---
+++ trunk/BPMProject/BPM.BLL/EquipmentRepairMng.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.OrmLite;
+using BPM.Entity;
+using BPM.Entity.DTO;
+using BPM.Entity.Paged;
+using BPM.ORMLite;
+using BPM.Entity.Proccess;
+namespace BPM.BLL
+{
+    public class EquipmentRepairMng
+    {
+        /// <summary>
+        /// 装备进场
+        /// </summary>
+        /// <param name="er"></param>
+        /// <returns></returns>
+        public static long EquipmentInputFactory(EquipmentRepair er)
+        {
+            return Utity.Connection.Insert<EquipmentRepair>(er, selectIdentity: true);
+        }
+
+    }
+}
---
+++ trunk/BPMProject/BPM.Entity/Proccess/EquipmentRepair.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPM.Entity.Proccess
+{
+    /// <summary>
+    /// 装备维修实体
+    /// </summary>
+    public class EquipmentRepair
+    {
+        public long id { get; set; }
+        public double equipmentid { get; set; }
+        public string equipmentname { get; set; }
+        public string associateTime { get; set; }
+        public string associateLoc { get; set; }
+        public string associateLog { get; set; }
+        public string attachLog { get; set; }
+        public string docLog { get; set; }
+
+        public int preRepairLevel { get; set; }
+        public string preRepairTime { get; set; }
+        public string createTime { get; set; }
+        public string inputFactoryTime { get; set; }
+        public string userLog { get; set; }
+        public string repairAdvice { get; set; }
+        public string inquestUnit { get; set; }
+        public string inquestUser { get; set; }
+        public string repairRequire { get; set; }
+        public int    repairMakeLevel { get; set; }
+        public string makeLevelReason { get; set; }
+        public string checkTask { get; set; }
+        public string kpi { get; set; }
+        public string checkLoc { get; set; }
+        public string checkMethod { get; set; }
+        public string armyCheck { get; set; }
+        public string armyResult { get; set; }
+        public string equipmentResult { get; set; }
+        public string armyManResult { get; set; }
+        public string equipmentConsignLog { get; set; }
+        public string attachConsignLog { get; set; }
+        public string docConsignLog { get; set; }
+    }
+}
